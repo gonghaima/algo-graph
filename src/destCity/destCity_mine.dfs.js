ca5214a9fc24424c1a7aa28ae7bca229248622ce
Steven <gonghaima@hotmail.com>
2020-07-25 10:53:30 +1000

    dfs - mine - test case passes

--- src/destCity/destCity_mine.dfs.js
+++ src/destCity/destCity_mine.dfs.js
@@ -2,23 +2,17 @@ export default paths => {
     const visited = new Set();
     let next = paths;
     visited.add(paths[0]);
-    let dest = paths[0][1];
-    let keepChecking = true;
+    let destination = paths[0][1];
+    const dfs = (node, checked) => {
+        const next = (paths.filter(([s, e]) => node[1] === s))[0];
+        if (next && !checked.has(next)) {
+            checked.add(next);
+            destination = next[1];
+            dfs(next, checked);
+        }
+    };
 
-    while (next.length > 0 && keepChecking) {
-        let toBeVisited = [];
-        next.map(path => {
-            if (!visited.has(path)) {
-                if (path[0] === dest) {
-                    dest = path[1];
-                    visited.add(path);
-                } else {
-                    toBeVisited.push(path);
-                }
-            }
-        })
-        if (next.length === toBeVisited.length) keepChecking = false;
-        next = toBeVisited;
-    }
-    return dest;
+    dfs(paths[0], visited);
+
+    return destination;
 };
