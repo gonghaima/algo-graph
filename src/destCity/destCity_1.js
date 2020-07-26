c0df2cd553993e058882df42e9bd180ef2bed5c5
Steven <gonghaima@hotmail.com>
2020-07-26 21:26:25 +1000

    notes update

--- src/destCity/destCity_1.js
+++ src/destCity/destCity_1.js
@@ -1,4 +1,5 @@
 // Filter paths to arrivals and destinations, then filter to the destination not found in arrivals.
+
 // time 65.79%, space 45.69%
 export default paths => {
     const arrivals = paths.map(a => a[0]);
