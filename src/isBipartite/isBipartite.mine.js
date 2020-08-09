50fa48ac4c12f2a8d781acee816780c44475b4e3
Steven <gonghaima@hotmail.com>
2020-08-09 11:18:32 +1000

    first solution

--- src/isBipartite/isBipartite.mine.js
+++ src/isBipartite/isBipartite.mine.js
@@ -1,81 +1,99 @@
+// const split = arr => {
+//     let result = [];
+//     console.log(`arr is :${JSON.stringify(arr)}`);
+//     for (let i = 0; i < arr.length; i++) {
+//         // console.log(i);
+//         let tmpArr = arr;
+//         let arrToEval;
 
-// export default graph => {
-//     const numOfNodes = graph.length;
-//     for (let idx = 0; idx < numOfNodes; idx++) {
-//         for (let i = idx + 1; i < numOfNodes; i++) {
-//             //graph[idx]  graph[i]
+//         if (i === 0) {
+//             arrToEval = tmpArr;
+//         } else {
+//             let headingNum = tmpArr.pop();
+//             arrToEval = [headingNum].concat(tmpArr);
 //         }
+
+//         for (let idx = i; idx < arr.length; idx++) {
+//             console.log(`inside of idx ${idx}`);
+//             let group1 = arrToEval.slice(0, idx + 1);
+//             let group2 = arrToEval.slice(idx + 1, arrToEval.length);
+//             if (group1.length !== arrToEval.length && group2.length !== arrToEval.length) result.push([group1, group2]);
+//         }
+
 //     }
-//     return true;
-// };
-
-
-// const takeAllPossibleCombination = (data, numToTake)=>{
-// // [{wanted:[],rest:[]}]
-
+//     return result;
 // }
 
+// https://stackoverflow.com/questions/31702427/find-all-combinations-of-two-arrays
 
-// const splitToTwoGroups = (data) => {
-//     // data =  [1,2,3,4,5,6,7]
-//     // [{wanted:[],rest:[]}]
-//     const cnt = data.length;
-//     for (let index = 0; index < cnt; index++) {
-//         const element = array[index];
-//         // all possibilities for data[index]
-//     }
-
-// }
-
-
-// export default graph => {
-//     const numOfNodes = graph.length;
-//     const lpCount = Math.ceil(numOfNodes / 2);
-//     for (let idx = 1; idx < lpCount; idx++) {
-//         for (let i = idx + 1; i < numOfNodes; i++) {
-//             //graph[idx]  graph[i]
-//         }
-//     }
-//     return true;
-// };
-
-
-
-const split = arr => {
-    let result = [];
-    console.log(`arr is :${JSON.stringify(arr)}`);
-    for (let i = 0; i < arr.length; i++) {
-        // console.log(i);
-        let tmpArr = arr;
-        let arrToEval;
-        // console.log(`in loop now`);
-        // console.log(tmpArr);
-        // console.log(`after tmpArr`);
-
-        if (i === 0) {
-            arrToEval = tmpArr;
-        } else {
-            let headingNum = tmpArr.pop();
-            arrToEval = [headingNum].concat(tmpArr);
-        }
-
-        for (let idx = i; idx < arr.length; idx++) {
-            console.log(`inside of idx ${idx}`);
-            let group1 = arrToEval.slice(0, idx + 1);
-            let group2 = arrToEval.slice(idx + 1, arrToEval.length);
-            if (group1.length !== arrToEval.length && group2.length !== arrToEval.length)result.push([group1, group2]);
-        }
-
+function clone(arr) {
+    var copy = new Array(arr.length);
+    for (var i = 0; i < arr.length; i++) {
+        copy[i] = new Array();
+        for (var j = 0; j < arr[i].length; j++)
+            copy[i][j] = arr[i][j];
     }
+
+    return copy;
+}
+
+function split(left, right) {
+    var result = [];
+
+    function _f(left, i) {
+        if (i == right.length) {
+            result.push(left);
+            return;
+        }
+
+        for (var j = 0; j < left.length; j++) {
+            var _left = clone(left);
+            _left[j].push(right[i]);
+            _f(_left, i + 1);
+        }
+    }
+
+    _f(left, 0);
     return result;
 }
 
+
 export default graph => {
+    let isBipartite = false;
+
     const lg = graph.length;
     const nodes = [];
     for (let i = 0; i < lg; i++) {
         nodes.push(i);
     }
-    const groups = split(nodes);
-    return groups;
+
+    // get all possible two groups
+    const groups = split([[], []], nodes);
+
+    // return groups;
+
+    // confirm each edge can reach two groups
+    // get each groups
+    for (let i = 0; i < groups.length && isBipartite === false; i++) {
+        let cB = true;
+        const [s, e] = groups[i];
+
+        // check if every edge is included in the group
+        for (let graphIdx = 0; graphIdx < graph.length && cB; graphIdx++) {
+            const edgeList = graph[graphIdx];
+            // let isB = true;
+            for (let edIdx = 0; edIdx < edgeList.length; edIdx++) {
+                const edgeEnd = edgeList[edIdx];
+                // [idx, edgeEnd]
+                let y = s.includes(graphIdx) && e.includes(edgeEnd) || s.includes(edgeEnd) && e.includes(graphIdx);
+                if (y === false) {
+                    debugger;
+                    cB = false;
+                }
+            }
+        }
+        if (cB) isBipartite = true;
+    }
+
+    return isBipartite;
 };
