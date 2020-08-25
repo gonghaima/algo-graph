a848bcac5f06d6316d739e69782b5a1f97154b4e
Steven <gonghaima@hotmail.com>
2020-08-25 13:10:54 +1000

    bfs-1

---
+++ src/isBipartite/isBipartite_bfs_1.js
@@ -0,0 +1,24 @@
+export default graph => {
+    const colors = new Map();
+    const stack = [];
+
+    for (let i = 0; i < graph.length; i++) {
+        if (colors.has(i)) continue;
+
+        colors.set(i, true);
+        stack.push(i);
+
+        while (stack.length > 0) {
+            let current = stack.pop();
+
+            for (let neighbour of graph[current]) {
+                if (colors.get(neighbour) === colors.get(current)) return false;
+                if (!colors.has(neighbour)) {
+                    colors.set(neighbour, !colors.get(current));
+                    stack.push(neighbour);
+                }
+            }
+        }
+    }
+    return true
+};
